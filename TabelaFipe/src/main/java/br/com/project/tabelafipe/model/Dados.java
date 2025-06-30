fb1dbcb20c6f97c574bc3173451b00f1b8cb50f0
unknown <enzo_fischer10@hotmail.com>
2025-06-29 22:05:11 -0300

    Deploying Architecture

---
+++ TabelaFipe/src/main/java/br/com/project/tabelafipe/model/Dados.java
@@ -0,0 +1,4 @@
+package br.com.project.tabelafipe.model;
+
+public record Dados(String codigo, String nome) {
+}
---
+++ TabelaFipe/src/main/java/br/com/project/tabelafipe/service/IConverteDados.java
@@ -0,0 +1,4 @@
+package br.com.project.tabelafipe.service;
+
+public class IConverteDados {
+}
